040225214c1f9871f340083ae0888d3edd5dcf8d
Mahmud Hasan <Mahmud256>
2023-11-24 18:41:45 +0600

    Include Footer

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Header/Navbar';
+import Footer from '../page/Footer/Footer';
 
 const Layout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
