938d6a41855c2fb8ee7bc668b2e90bcfedcf3953
Mahmud Hasan <Mahmud256>
2023-11-24 12:38:10 +0600

    Include Banner

--- src/main.jsx
+++ src/main.jsx
@@ -10,5 +10,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
